3711aa76e3ca7bff9671f1f7d90b5c0e9b9d3e32
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-13 13:47:42 +0600

    CubeVolumeCalculator

---
+++ src/main/java/ru/danilakondr/volumes/CubeVolumeCalculator.java
@@ -0,0 +1,29 @@
+package ru.danilakondr.volumes;
+
+import java.math.BigDecimal;
+
+/**
+ * Класс, вычисляющий объём куба. Объём куба вычисляется по
+ * формуле <code>$a^3$</code>.
+ * <p>
+ * Принимает параметры:
+ * <ul>
+ * <li><code>a</code> &mdash; сторона куба
+ * </ul>
+ * 
+ * @author Данила А. Кондратенко
+ * @see Calculator
+ */
+public class CubeVolumeCalculator extends Calculator {
+
+	public CubeVolumeCalculator() {
+		super();
+	}
+
+	@Override
+	public BigDecimal calculate() {
+		BigDecimal a = getParameter("a");
+		
+		return a.pow(3);
+	}
+}
