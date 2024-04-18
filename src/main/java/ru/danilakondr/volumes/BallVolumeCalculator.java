332618f939943e9d8efbe673698b014ce7bd468d
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-18 20:11:43 +0600

    BallVolumeCalculator: увеличение точности отображения и Javadoc

--- src/main/java/ru/danilakondr/volumes/BallVolumeCalculator.java
+++ src/main/java/ru/danilakondr/volumes/BallVolumeCalculator.java
@@ -4,6 +4,21 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 
+/**
+ * Класс, вычисляющий объём шара. Объём шара вычисляется по формуле
+ * <code>$\frac43 \pi R^3$</code>.
+ * <p>
+ * Принимает параметры:
+ * <ul>
+ * <li><code>R</code> &mdash; радиус шара
+ * </ul>
+ * <p>
+ * При отображении используется точность в 20 значащих цифр, при
+ * вычислении &mdash; в 40.
+ * 
+ * @author Данила А. Кондратенко
+ * @since 0.2.1
+ */
 public class BallVolumeCalculator extends Calculator {
 	@SuppressWarnings("unused")
 	private static final String[] PARAMETERS = new String[] {"R"};
@@ -14,7 +29,7 @@ public class BallVolumeCalculator extends Calculator {
 	
 	private static final BigDecimal PI = new BigDecimal("3.1415926535897932384626433832795028841971");
 	private static final int CALC_SCALE = 40;
-	private static final int REPR_SCALE = 10;
+	private static final int REPR_SCALE = 20;
 	
 	private static final MathContext CALC_CONTEXT = new MathContext(CALC_SCALE, RoundingMode.HALF_EVEN);
 	private static final MathContext REPR_CONTEXT = new MathContext(REPR_SCALE, RoundingMode.HALF_EVEN);
